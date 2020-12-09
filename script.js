aaab21ca2d5d15019bf9814f1f06e1a7cbf4b3c8
SolonsList <73976449+SolonsList@users.noreply.github.com>
2020-12-08 20:27:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //v3.4 Add popup describing app when visitors load webpage the first time
 window.onload = function() {
-    alert("Welcome to 'Katy's Compliment List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
+    alert("Welcome to 'Katy's Compliment List' App!\n\nGive someone a compliment!\n**Check mark the box when the compliment is completed.**\n\nClick remove to delete a compliment.");
     populateshoppinglistonload();
     displayShoppinglists();
     clearFocus();
